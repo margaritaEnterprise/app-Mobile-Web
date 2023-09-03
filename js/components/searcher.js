39c0ca561eda1d675f13b698f545ae11624d47b7
veroyols <vero.scholz@gmail.com>
2023-09-03 00:45:26 -0300

    searcher

---
+++ js/components/searcher.js
@@ -0,0 +1,17 @@
+
+const Aside = () => {
+
+    return  `
+    <div class="aside">
+        <i class="material-icons">search</i>
+        <label type="region">Selecciona un continente:</label>
+    <select>
+        <option>America</option>
+        <option>Europa</option>
+        <option>Asia</option>
+    </select>
+    </div>
+    `
+}
+        
+export default Footer; 
