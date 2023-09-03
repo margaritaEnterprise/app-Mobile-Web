c940242ff1cdb108162eba31ca652499cf3e2ab5
NicoDVKa <herlannicolas6@gmail.com>
2023-09-03 17:51:04 -0300

    search styles

--- js/components/searcher.js
+++ js/components/searcher.js
@@ -1,39 +1,73 @@
-
 const Searcher = () => {
-
     const continents = [
-        { value: null, name:""},
-        { value: "Africa", name: "África" },
-        { value: "Americas", name: "América" },
-        { value: "Antarctica", name: "Antártida" },
-        { value: "Asia", name: "Asia" },
-        { value: "Europe", name: "Europa" },
-        { value: "Cceania", name: "Oceanía" },
+      { value: null, name: "" },
+      { value: "Africa", name: "África" },
+      { value: "Americas", name: "América" },
+      { value: "Antarctica", name: "Antártida" },
+      { value: "Asia", name: "Asia" },
+      { value: "Europe", name: "Europa" },
+      { value: "Oceania", name: "Oceanía" }, // Corregido "Cceania" a "Oceania"
     ];
+  
+    const regions = [
+      { value: null, name: "" },
+      { value: "Africa", name: "África" },
+      { value: "Americas", name: "América" },
+      { value: "Antarctica", name: "Antártida" },
+      { value: "Asia", name: "Asia" },
+      { value: "Europe", name: "Europa" },
+      { value: "Oceania", name: "Oceanía" }
+    ]
 
-    return  `
-    <div class="aside">
-        <form>
-            <input type="search" placeholder="Ingrese un nombre . . . ">
-            <button type="submit"><i class="material-icons">search</i></button>
-<br>
-            <label value="region">Continente:</label>
-            <select>
-            ${continents.map((continent) =>
-                `<option value:"${continent.value}">${continent.name}</option>`
-            ).join()}
-            </select>
-<br>
-
-            <label value="population">
-                Cantidad minima de habitantes (1-1 400 000 000):
-            </label>
-            <input type="range" min="1" max="1400000000">
-            <output>1</output>
+    return `
+      <div class="searcher">
+      <form class="searcher__form">
+        <div class="search__header">
+        <input
+        type="search"
+        class="searcher__input searcher__input--inline"
+        placeholder="Ingrese un nombre . . ."
+        />
+        <button type="submit" class="searcher__button">
+            <i class="material-icons">search</i>
+        </button>
+        </div>  
+          <br />
+          <label for="region" class="searcher__label">Continente:</label>
+          <select id="region" class="searcher__select">
+            ${continents
+              .map(
+                (continent) =>
+                  `<option value="${continent.value}">${continent.name}</option>`
+              )
+              .join("")}
+          </select>
+          <br />
+          <label for="subregion" class="searcher__label">Region: </label>
+          <select id="subregion" class="searcher__select">
+            ${regions
+              .map(
+                (regions) =>
+                  `<option value="${regions.value}">${regions.name}</option>`
+              )
+              .join("")}
+          </select>
+          <br />
+          <label for="population" class="searcher__label">
+            Cantidad mínima de habitantes (1-1 400 000 000):
+          </label>
+          <input
+            type="range"
+            id="population"
+            class="searcher__range"
+            min="1"
+            max="1400000000"
+          />
+          <output class="searcher__output">1</output>
         </form>
-
-    </div>
-    `
-}
-        
-export default Searcher; 
+      </div>
+    `;
+  };
+  
+  export default Searcher;
+  
