37dfe2da61a49b8963bebda444005b57e85918ec
NicoHerlan <nicodvkcss@gmail.com>
2023-09-03 12:28:45 -0300

    add card styles

--- js/components/card.js
+++ js/components/card.js
@@ -3,13 +3,16 @@ const Card = (country) => {
 
     return  `
     <div class="card">
-        <img src=${country.flags.png} alt="Bandera de ${country.flags.alt}">
-        <h2>${country.name.common}</h2>
-        <p>Continente: ${country.region}</p>
-        <a href=""><i class="material-icons">share</i></a>
-        <a href=""><i class="material-icons">stars</i></a>
-        <a href=""><i class="material-icons">star_rate</i></a>
+        <img src="${country.flags.png}" alt="Bandera de ${country.flags.alt}" class="card__image">
+        <h2 class="card__title">${country.name.common}</h2>
+        <p class="card__info">Continente: ${country.region}</p>
+        <div class="card__links">
+            <a href="#" class="card__link"><i class="material-icons">share</i></a>
+            <a href="#" class="card__link"><i class="material-icons">stars</i></a>
+            <a href="#" class="card__link"><i class="material-icons">star_rate</i></a>
+        </div>
     </div>
+
     `
 }
         
