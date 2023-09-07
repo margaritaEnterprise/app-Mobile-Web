fb85eac737e487a0b3a0a8a8f5087a09a3e1a0b8
veroyols <vero.scholz@gmail.com>
2023-09-06 22:48:18 -0300

    Header text

--- js/components/header.js
+++ js/components/header.js
@@ -6,32 +6,30 @@ const Header = (page) =>{
             <i class="material-icons header__logoIcon">language</i>
             <h1>WebApp</h1>
         </div>       
-         <ul class="header__navList" id="navList">
+        <ul class="header__navList" id="navList">
             <li class="header__navItem ${page === "home"? "header__navItem--selected" : ""}" title="Inicio" id="home">
-                <a href="./../pages/index.html">
-                    <i class="material-icons">home</i>
-                </a>
+                <a href="./../pages/index.html"><button><i class="material-icons">home</i>Inicio</button></a>
             </li>
             <li class="header__navItem ${page === "favorites"? "header__navItem--selected" : ""}" title="Favoritos" id="star">
-                <a href="./../pages/favorites.html">
+                <a href="./../pages/favorites.html"><button>
                     <i class="material-icons">star</i>
                     <!-- <i class="header__counter">(0)</i> -->
-                </a>
+                Favoritos</button></a>
             </li>
             <li class="header__navItem ${page === "history"? "header__navItem--selected" : ""}" title="Historial" id="history">
-                <a href="./../pages/history.html"> 
+                <a href="./../pages/history.html"><button> 
                     <i class="material-icons">history</i>
-                </a>
+                Historial</button></a>
             </li>
             <li class="header__navItem ${page === "games"? "header__navItem--selected" : ""}" title="Juegos" id="game">
-                <a href="./../pages/games.html">
+                <a href="./../pages/games.html"><button>
                     <i class="material-icons">sports_esports</i>
-                </a>
+                Juegos</button></a>
             </li>
             <li class="header__navItem ${page === "aboutUs"? "header__navItem--selected" : ""}" title="Nosotros" id="game">
-                <a href="./../pages/aboutUs.html">
+                <a href="./../pages/aboutUs.html"><button href="./../pages/aboutUs.html">
                     <i class="material-icons">diversity_3</i>
-                </a>
+                Nosotros</button></a>
             </li>    
         </ul>
     </nav>
