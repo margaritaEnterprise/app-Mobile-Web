57208de5af228d194dbca05b4084b3092b0ad372
veroyols <vero.scholz@gmail.com>
2023-09-06 21:01:14 -0300

    Footer mobile

--- js/components/footer.js
+++ js/components/footer.js
@@ -3,39 +3,43 @@ const Footer = () => {
 
     return  `
     <div class="footer__body">
-    <div class="footer__icons">
-        <h3 class="footer__title">Síguenos:</h3>
-        <a href="#" title="Facebook" class="footer__icon"><i class="fab fa-facebook"></i></a>
-        <a href="#" title="LinkedIn" class="footer__icon"><i class="fab fa-linkedin"></i></a>
-        <a href="#" title="Twitter" class="footer__icon"><i class="fab fa-twitter"></i></a>
-        <a href="#" title="Instagram" class="footer__icon"><i class="fab fa-instagram"></i></a>
-        <a href="#" title="Youtube" class="footer__icon"><i class="fab fa-youtube"></i></a>
-        <br>
-        <a href="#" title="WebApp"> 
-            <i class="material-icons">language</i>
-        </a>
-        <h3 class="footer__title">WebApp</h3>
+
+        <div class="footer__aboutUs">
+            <h3>Nosotros</h3>
+            <p>
+                Somos lalala lalala lalala lalala lalala lalala lalala lalala lalala lalala 
+                lalala lalala. 
+                <a href="./../pages/aboutUs.html">Leer mas..</a>
+            </p>       
+        </div>
+    
+        <div class="footer__site">
+            <a href="./../pages/index.html">Inicio</a> 
+            <a href="./../pages/favorites.html">Favoritos</a> 
+            <a href="./../pages/history.html">Historial</a> 
+            <a href="./../pages/games.html">Juegos</a> 
+            <a href="./../pages/aboutUs.html">Nosotros</a> 
+        </div>
+
+        <div class="footer__network">
+            <a href="#" title="Facebook"><i class="fab fa-facebook"></i></a>
+            <a href="#" title="LinkedIn"><i class="fab fa-linkedin"></i></a>
+            <a href="#" title="Twitter"><i class="fab fa-twitter"></i></a>
+            <a href="#" title="Instagram"><i class="fab fa-instagram"></i></a>
+            <a href="#" title="Youtube"><i class="fab fa-youtube"></i></a>
+        </div>
+        <div class="footer__logo">
+            <a href="#" title="WebApp"> 
+                <i class="material-icons">language</i>
+            </a>
+            <h3>WebApp</h3>
+        </div>
     </div>
     
-    <div class="footer__hour">
-            <a href="./../pages/index.html">Inicio</a> |
-            <a href="./../pages/favorites.html">Favoritos</a> |
-            <a href="./../pages/history.html">Historial</a> |
-            <a href="./../pages/games.html">Juegos</a> |
-            <a href="./../pages/aboutUs.html">Nosotros</a> 
-        <p class="footer__title">© 2023 | Aplicaciones Moviles </p>
+    <div class="footer__footer">
+        <a href='#' id="footer_up"><i class="material-icons" title="Inicio">keyboard_double_arrow_up</i></a>
+        <p>© 2023 | Aplicaciones Moviles </p>
     </div>
-    <div class="footer__contact">
-        <h3 class="footer__title">Nosotros</h3>
-        <p class="footer__text">
-            Somos lalala lalala lalala lalala lalala lalala lalala lalala lalala lalala 
-            lalala lalala. <a href="./../pages/aboutUs.html" class="footer__link">Leer mas..</a>
-        </p>       
-    </div>
-</div>
-<div class="footer__footer">
-    <a href='#' id="footer__up" class="footer__link"><i class="material-icons" title="Inicio">keyboard_double_arrow_up</i></a>
-</div>
     `
 }
         
