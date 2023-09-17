60bbcf9ca8c883f0041b978aa9dce8e4fb99478e
NicoHerlan <nicodvkcss@gmail.com>
2023-09-17 15:34:57 -0300

    add spinner

--- js/init.js
+++ js/init.js
@@ -3,16 +3,37 @@ import renderSearcher from "./actions/renderSearcher.js";
 import renderCountries from "./actions/renderCountries.js";
 import renderFooter from "./actions/renderFooter.js";
 import { getCountries } from "./services/countries.js";
+import { renderLoader } from "./actions/renderLoader.js";
+
 
 const init = async () => {
+
     renderHeader("home");
     renderSearcher();
     renderFooter();
-    
-    let countries = await getCountries();
+
     let section = document.getElementById('all');
+    let loaderContainer = document.getElementById('loader-container');
+
+    renderLoader(loaderContainer);
+    let countries = await getCountries();
     renderCountries(section, countries); //usar constantes
+
+    $("#loader-container").css("display", "none");
 }
 
-window.onload = init; 
-//onload el dom ya se creo en el navegador
+init();
+    
+$(document).ready(function() {
+
+
+
+
+    $(".searcher__form").submit(function(event) {
+        event.preventDefault();
+
+    });
+});
+
+
+
