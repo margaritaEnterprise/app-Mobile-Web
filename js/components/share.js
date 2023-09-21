4fec6a48c7fe8ba52f9b9b7abd92a4ba5a136965
veroyols <vero.scholz@gmail.com>
2023-09-21 16:00:50 -0300

    validacion y compartir

--- js/components/share.js
+++ js/components/share.js
@@ -9,7 +9,7 @@ const Share = () => {
                 id="share__emailMe" 
                 class="email-form__input" 
                 placeholder="Ingrese su email . . ."/>
-                <p id="info__emailMe" style="color: red">Ingrese un mail valido</p>
+                <p id="info__emailMe" style="color: red">*Ingrese un mail valido</p>
         </div>
         <div>
             <label for="share__name" class="email-form__label">Nombre del destinatario:</label>
@@ -20,7 +20,7 @@ const Share = () => {
                 placeholder="Nombre del destinatario . . ."
                 minlength="1" 
                 maxlength="50" />
-                <p id="info__name" style="color: red">Ingrese un nombre valido</p>
+                <p id="info__name" style="color: red">*Ingrese un nombre valido</p>
 
         </div>
         <div>
@@ -30,24 +30,21 @@ const Share = () => {
                 id="share__emailYou" 
                 class="email-form__input" 
                 placeholder="Correo del destinatario . . ."/>
+                <p id="info__emailYou" style="color: red">*Ingrese un mail valido</p>
         </div>
         <div>
             <label for="message" class="email-form__label">Mensaje (opcional):</label>
             <textarea 
                 id="share__message" 
                 class="email-form__textarea" 
-                placeholder="Mensaje">
-            </textarea>
+                placeholder="Mensaje"></textarea>
         </div>
         <div class="email-form__field">
-            <button id="submit__form__button" type="submit" class="email-form__submit">Compartir</button>
+            <button id="submit__form__button" type="submit" class="email-form__submit" disabled>Compartir</button>
         </div>
     </form>
     `;
 };
 
-//const submitShare = (code) =>{//onclick="submit('${code}')" 
-    //validar 
-//}window.submitShare = submitShare;
 
 export default Share;
--- js/events/submitShare.js
+++ js/events/submitShare.js
@@ -1,34 +1,71 @@
 import {validateEmail, validateText} from "../utils/validate.js"
+import { getCountriesByCodes } from "../services/countries.js";
+
+const showInfo = (formSection, infoSection, validate) => {
+    return new Promise((resolve) => {
+        $(formSection).keyup((e) => {
+            let boo = validate(e.target.value);
+            if (boo) {
+                $(formSection).prop("isValid", true);
+                $(infoSection).hide();
+            } else {
+                $(formSection).prop("isValid", false);
+                $(infoSection).show();
+            }
+            resolve(); // Resuelve la promesa cuando se complete la validación
+        });
+    });
+}
 
 const submitShare = () => {
-    $("#share__emailMe").keyup((e) => { 
-        let boo = validateEmail(e.target.value)
-        console.log(boo)
-        console.log(e.target.value +" -> "+boo )
-        if(boo){
-            $("#info__emailMe").hide();
-        } else {
-            $("#info__emailMe").show();
-        }
-    });
-    $("#share__name").keyup((e) => { 
-        let boo = validateText(e.target.value)
-        console.log(e.target.value +" -> "+boo )
-        if(!boo){
-            $("#info__name").show();
-        } else {
-            $("#info__name").hide();
-        }
-    });
-    $("#share__emailYou").keyup((e) => { 
-        let boo = validateEmail(e.value)
-        console.log(e.target.value +" -> "+boo )
-    });
-    $("#share__message").keyup((e) => { 
-        let boo = validateText(e.target.value)
-        console.log(e.target.value +" -> "+boo )
-    });
-    
+    let emailMe = $("#share__emailMe");
+    let infoEmailMe = $("#info__emailMe");
+    let emailYou = $("#share__emailYou");
+    let infoEmailYou = $("#info__emailYou");
+    let name = $("#share__name");
+    let infoName = $("#info__name");
+    let message = $("#share__message");
+
+    Promise.all([
+        showInfo(emailMe, infoEmailMe, validateEmail),
+        showInfo(emailYou, infoEmailYou, validateEmail),
+        showInfo(name, infoName, validateText)
+    ]).then(() => {
+
+            let isValidMailMe = $(emailMe).prop("isValid");
+            let isValidMailYou = $(emailYou).prop("isValid");
+            let isValidMailName = $(name).prop("isValid");
+
+            let boo = isValidMailMe && isValidMailYou && isValidMailName
+
+            if(boo) {
+                let button = $("#submit__form__button")
+                $(button).prop("disabled", false);
+                $(button).click(async (e) => { 
+                    e.preventDefault();
+
+                    
+                    const urlActual = window.location.href;
+                    const params = new URLSearchParams(new URL(urlActual).search);
+                    const code = params.get('code');
+                    let country = await getCountriesByCodes([code]);
+                    const link = `https://app-mobile-web-sxwq.vercel.app/pages/detail.html?code=${code}`
+                    /*const html = `<div>
+                                        <img src="${country[0].flags.png}" alt="Bandera de ${country[0].flags.alt}"/>
+                                        <h2>${country[0].translations.spa.common}</h2>
+                                        <p>Continente: ${country[0].region}</p>
+                                    </div>`*/
+                    let body = `Hola ${name.val()}! 
+                        ${message ? message.val() : ''}
+                        Haz click en el siguiente link para ver la información del país 
+                        ${encodeURIComponent(link)}`
+                    let send = `mailto:${emailYou.val()}?subject=Tienes un mensaje de Country Web&body=${body}`
+                    window.location.href = send;                   
+                });
+            }
+        })
+
 }
 
 export default submitShare;
+
