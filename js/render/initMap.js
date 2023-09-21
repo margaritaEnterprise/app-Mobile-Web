987f5c1f9fb67cfa87ca724ed9568b329c3601b2
NicoHerlan <nicodvkcss@gmail.com>
2023-09-20 23:47:47 -0300

    change map api (leaflet)

--- js/render/initMap.js
+++ js/render/initMap.js
@@ -1,18 +1,16 @@
 const initMap = (lat,long,area) => {
-  console.log(lat,long,area);
 
-  let logBase10 = Math.log(area) / Math.log(10)
-  let zoom = Math.floor( Math.log(area) / (logBase10));
-  var coord = {lat:lat ,lng: long};
-  
-  var map = new google.maps.Map(document.getElementById('map'),{
-    zoom: zoom, //unaj
-    center: coord
-  });
-  var marker = new google.maps.Marker({
-    position: coord,
-    map: map
-  });
+    var mymap = L.map('map').setView([lat, long], 10);
+
+  // Agrega una capa de mapa base
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap',
+    maxZoom: 18,
+}).addTo(mymap);
+
+  var marker = L.marker([lat, long]).addTo(mymap);
+
+  L.control.scale().addTo(mymap);
 }
 
 export default initMap;
